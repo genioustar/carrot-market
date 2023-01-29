df68af3b77d405f8e4f76303fd062f110c84a2df
genioustar <genioustar@gmail.com>
2023-01-30 04:35:05 +0900

    community 완성 chat 완성!

---
+++ pages/chats/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+
+const Chat: NextPage = () => {
+  return (
+    <div className="divide-y-[1px] py-10">
+      {[1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          key={i}
+          className="flex cursor-pointer items-center space-x-3 py-3 px-4"
+        >
+          <div className="h-10 w-10 rounded-full bg-slate-300" />
+          <div>
+            <p className="text-gray-700">Steve Jebs</p>
+            <p className="text-xs font-medium text-gray-500">
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Chat;
