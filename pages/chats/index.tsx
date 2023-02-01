fed1112e6b1d4fa74119aa0be6b27ccf15eb4763
genioustar <genioustar@gmail.com>
2023-02-01 10:08:45 +0900

    세밀한 업데이트

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,23 @@
 import Layout from "@/components/layout";
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Chat: NextPage = () => {
   return (
     <Layout title="메세지" hasTabBar>
       <div className="divide-y">
         {[1, 1, 1, 1, 1, 1].map((_, i) => (
-          <div
-            key={i}
-            className="flex cursor-pointer items-center space-x-3 py-3 px-4"
-          >
-            <div className="h-10 w-10 rounded-full bg-slate-300" />
-            <div>
-              <p className="text-gray-700">Steve Jebs</p>
-              <p className="text-xs font-medium text-gray-500">
-                See you tomorrow in the corner at 2pm!
-              </p>
+          <Link key={i} href={`/chats/${i}`}>
+            <div className="flex cursor-pointer items-center space-x-3 py-3 px-4">
+              <div className="h-10 w-10 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-xs font-medium text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </Layout>
