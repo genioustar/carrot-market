3e67ab4a3ab7234c642ba37802f28af9590e1710
genioustar <4574gkdl>
2023-02-08 15:58:04 +0900

    token 만들어서 UI까지 적용하는거 완료!

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -7,20 +7,26 @@ import { useState } from "react";
  * 받아서 해당 데이터를 state 변수에 넣는다.
  * 이후 return으로 fetch하는 함수와 fetch된 결과를 보내준다.
  */
+
+// data가 어떤 타입이던 받을 수 있기 때문에! <T>로 interface에 제너릭 설정해야하고
+interface UseMutationState<T> {
+  loading: boolean;
+  data?: T;
+  error?: object;
+}
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
+
 // POST에 대한것만 커스텀훅을 만드는 이유는 GET은 SWR을 통해서 어메이징한 것들을 할 수 있기 때문이다!
-export default function useMutation(
+export default function useMutation<T = any>(
   url: string
-): [
-  (data?: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
-  const [states, setStates] = useState({
+): UseMutationResult<T> {
+  const [state, setState] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
   });
-  function mutation(data?: any) {
-    setStates((prev) => ({ ...prev, loading: true }));
+  function mutation(data: any) {
+    setState((prev) => ({ ...prev, loading: true }));
     fetch(url, {
       method: "POST",
       headers: {
@@ -29,9 +35,9 @@ export default function useMutation(
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {})) // catch가 있는건 response.json()이 동작하지 않을때가 있음.
-      .then((data) => setStates((prev) => ({ ...prev, data }))) // then((json)=>setDate(json)) 이랑 같은거!
-      .catch((err) => setStates((prev) => ({ ...prev, err })))
-      .finally(() => setStates((prev) => ({ ...prev, loading: false })));
+      .then((data) => setState((prev) => ({ ...prev, data }))) // then((json)=>setDate(json)) 이랑 같은거!
+      .catch((err) => setState((prev) => ({ ...prev, err })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
   }
-  return [mutation, states];
+  return [mutation, state];
 }
