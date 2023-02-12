715f7dc65025a1b89d611b0ddff22277d7b37c83
genioustar <4574gkdl>
2023-02-12 22:43:41 +0900

    router의 push, replace 메소드 차이

---
+++ libs/client/useUser.ts
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useUser() {
+  const [user, setUser] = useState();
+  const router = useRouter();
+  useEffect(() => {
+    fetch("/api/users/me") // /api/users/me 에서 데이터를 가져오고
+      .then((response) => response.json()) // response 받은 데이터를 가지고 json을 만들고
+      .then((json) => {
+        // 이 json을 가지고 json에 있는 값 중 ok가 true or false를 가지고 판단!
+        if (!json.ok) {
+          router.replace("/enter");
+        } else {
+          setUser(json.profile);
+        }
+      });
+  }, [router]); // router를 안 넣어줘도 되지만(절대 변경될리가 없어서... but 프로그램에서 워닝을 내줘서 넣어줌!)
+  return user;
+}
