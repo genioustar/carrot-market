910eb07e54489411d64dcfc4a5b819f6eb20c945
genioustar <genioustar@gmail.com>
2023-02-17 23:03:34 +0900

    community의 [id]에 따른 글 세부사항 작성 - 작성자 user 데이터 - question 데이터 - curiosity data - answers data

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,54 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withApiSession } from "@/libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req; // request에서 body에 있는 question을 question으로 받고, session에 있는 user를 user로 받기 위해서!
+  const post = await client?.post.findUnique({
+    where: { id: Number(id) },
+    include: {
+      // post와 연결된 user의 id와 name을 가져오는것! true로 하면되네??
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        // 이렇게 answer와 연관된 테이블의 정보를 가져오려면 일일이 select 로 가져와야 된다!
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          curiosity: true,
+        },
+      },
+    },
+  });
+  if (!post) res.status(404).json({ ok: false, error: "Not found post" }); // 404 not found 처리
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET"], handler }) // 3개이상의 파라미터일떄는 이렇게 객체로 전달해주는게 가독성에 좋음! fn:handler가 아니어도 되는건! 받는애가 handler라서 가능! isPrivate = true 해둬서 필수값이 아닌걸로 뽑아냄!
+);
--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,9 +1,54 @@
 import Button from "@/components/button";
 import Layout from "@/components/layout";
 import TextArea from "@/components/textarea";
+import { Answer, Post, User } from "@prisma/client";
 import type { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import useSWR from "swr";
+
+/**
+ * Answer table에 있는 user 정보의 관계를 가져오기 위해서 잇어야하는 extends 받은 AnswerWithUser interface
+ */
+interface AnswerWithUser extends Answer {
+  user: User;
+}
+
+/**
+ * Post table의 형태를 상속받는데 이 포스트는 user table의 정보를 가지고 있어서 이렇게 interface로 만들어야함
+ */
+interface PostWithUser extends Post {
+  user: User;
+  _count: {
+    answers: number;
+    curiosity: number;
+  };
+  answers: AnswerWithUser[]; // community/[id].ts 에서 answer이 응답으로 단순 데이터가아닌 relataion 테이블의 데이터를 가져옴으로 interface를 생성해줘야함.
+}
+
+/**
+ * 실제로 community/[id]에서 리턴하는 타입이 post 데이터 정보 + user 정보를 가지는 post 객체임으로
+ * 위에서 만든 Post를 extends 받은 PostWithUser interface 타입으로 선언한다.
+ */
+interface CoummunityPostREsponse {
+  ok: boolean;
+  post: PostWithUser;
+}
 
 const CommunityPostDetail: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useSWR<CoummunityPostREsponse>(
+    router.query.id ? `/api/posts/${router.query.id}` : null
+  ); // router의 query 값이 undefined일 수 있으므로 체크해주는 로직이 들어가야함.
+  console.log(data);
+  useEffect(() => {
+    // data가 없으면 이전 페이지로 가게하는 것!
+    if (data && !data.ok) {
+      router.push("/community");
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack>
       <div>
@@ -12,17 +57,19 @@ const CommunityPostDetail: NextPage = () => {
         </span>
         <div className="mb-3 flex cursor-pointer items-center space-x-3 border-b px-4 pb-3">
           <div className="h-10 w-10 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
+          <Link href={`/users/profiles/${data?.post.user.id}`}>
+            <p className="text-sm font-medium text-gray-700">
+              {data?.post.user.name}
+            </p>
             <p className="text-xs font-medium text-gray-500">
               View profile &rarr;
             </p>
-          </div>
+          </Link>
         </div>
         <div>
           <div className="mt-2 px-4 text-gray-700">
-            <span className="font-medium text-yellow-500">Q.</span> What is the
-            best mandu restaurant?
+            <span className="font-medium text-yellow-500">Q.</span>{" "}
+            {data?.post?.question}
           </div>
           <div className="mt-3 flex w-full space-x-5 border-t border-b-[2px] px-4 py-2.5  text-gray-700">
             <span className="flex items-center space-x-2 text-sm">
@@ -40,7 +87,7 @@ const CommunityPostDetail: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>궁금해요 1</span>
+              <span>궁금해요 {data?.post._count.curiosity}</span>
             </span>
             <span className="flex items-center space-x-2 text-sm">
               <svg
@@ -57,23 +104,23 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 1</span>
+              <span>답변 {data?.post._count.answers}</span>
             </span>
           </div>
         </div>
         <div className="my-5 space-y-5 px-4">
-          <div className="flex items-start space-x-3">
-            <div className="h-8 w-8 rounded-full bg-slate-200" />
-            <div>
-              <span className="block text-sm font-medium text-gray-700">
-                Steve Jebs
-              </span>
-              <span className="block text-xs text-gray-500 ">2시간 전</span>
-              <p className="mt-1 text-gray-700">
-                The best mandu restaurant is the one next to my house.
-              </p>
+          {data?.post.answers.map((answer) => (
+            <div key={answer.id} className="flex items-start space-x-3">
+              <div className="h-8 w-8 rounded-full bg-slate-200" />
+              <div>
+                <span className="block text-sm font-medium text-gray-700">
+                  {answer.user.name}
+                </span>
+                <span className="block text-xs text-gray-500 ">2시간 전</span>
+                <p className="mt-1 text-gray-700">{answer.answer}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className="px-4">
           <TextArea placeholder="Answer this question!" />
