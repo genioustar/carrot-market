c930aa12c56c6b7dac3ab4cadfa6771b32f45901
genioustar <4574gkdl>
2023-02-20 13:53:45 +0900

    useSWR에서 data변경 잘이뤄지고 이 데이터 변경에 따라서 useEffect 잘되는데 return에서 문져였던 post. -> post?. 으로 변경

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -65,9 +65,7 @@ const CommunityPostDetail: NextPage = () => {
     );
     curiosity({});
   };
-  console.log(data);
   useEffect(() => {
-    console.log("123123123", data);
     // data가 없으면 이전 페이지로 가게하는 것!
     if (data && !data.ok) {
       router.push("/community");
@@ -82,9 +80,9 @@ const CommunityPostDetail: NextPage = () => {
         </span>
         <div className="mb-3 flex cursor-pointer items-center space-x-3 border-b px-4 pb-3">
           <div className="h-10 w-10 rounded-full bg-slate-300" />
-          <Link href={`/users/profiles/${data?.post.user.id}`}>
+          <Link href={`/users/profiles/${data?.post?.user.id}`}>
             <p className="text-sm font-medium text-gray-700">
-              {data?.post.user.name}
+              {data?.post?.user.name}
             </p>
             <p className="text-xs font-medium text-gray-500">
               View profile &rarr;
@@ -118,7 +116,7 @@ const CommunityPostDetail: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>궁금해요 {data?.post._count.curiosity}</span>
+              <span>궁금해요 {data?.post?._count.curiosity}</span>
             </button>
             <span className="flex items-center space-x-2 text-sm">
               <svg
@@ -135,12 +133,12 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 {data?.post._count.answers}</span>
+              <span>답변 {data?.post?._count.answers}</span>
             </span>
           </div>
         </div>
         <div className="my-5 space-y-5 px-4">
-          {data?.post.answers.map((answer) => (
+          {data?.post?.answers.map((answer) => (
             <div key={answer.id} className="flex items-start space-x-3">
               <div className="h-8 w-8 rounded-full bg-slate-200" />
               <div>
