39abf4008915cda8c29756bb9fee795ee9f3f5f1
genioustar <4574gkdl>
2023-02-25 15:45:53 +0900

    - 나의 망고 페이지에서 보여줘야 할 favs, sales, purchases 정보들의 백엔드 완성

---
+++ pages/api/users/me/sales.ts
@@ -0,0 +1,33 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withApiSession } from "@/libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * 나의 관심상품을 모두 가져오는 부분
+ * @param req
+ * @param res
+ */
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+  const mysales = await client?.sale.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  res.json({
+    ok: true,
+    mysales,
+  });
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET"], handler }) // 3개이상의 파라미터일떄는 이렇게 객체로 전달해주는게 가독성에 좋음! fn:handler가 아니어도 되는건! 받는애가 handler라서 가능! isPrivate = true 해둬서 필수값이 아닌걸로 뽑아냄!
+);
