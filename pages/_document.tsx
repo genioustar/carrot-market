d59bb0cb76cd6d84c57f8ad3f15daa114b02080f
genioustar <4574gkdl>
2023-03-20 19:12:28 +0900

    - google-font를 이용한 font 커스터마이징의 최적화 지원(nextjs) - dynamic 에러 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
+/**
+ * 페이지를 랜더링하는데 사용되는 html 및 body 태그를 업데이트할 수 있는것!
+ * 여기에 설정하면 모든페이지에 적용되는것!
+ * 서버에서 한번만 실행됨. 따라서 onClick 이벤트 같은걸 추가할 수 없음!
+ * @returns
+ */
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        {/* 구글폰트일때만 해당되는 기능으로!! nextjs에서 빌드하고 앱을 배포할때 link에 존재하는 css를 다운로드해서 <style>안에 @font-face{...}으로 싹다 바꿔줌! */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
